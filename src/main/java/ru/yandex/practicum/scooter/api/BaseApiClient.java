b5b7ac02a71ae4742b280bd52f2466c037c1364a
Anastasiia Fokina <stasechka11@yandex.ru>
2022-06-03 13:44:44 +0300

    BaseApiClient

---
+++ src/main/java/ru/yandex/practicum/scooter/api/BaseApiClient.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.scooter.api;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+public class BaseApiClient {
+    public static final String BASE_URL = "https://qa-scooter.praktikum-services.ru";
+
+    public RequestSpecification getReqSpec() {
+        return new RequestSpecBuilder().log(LogDetail.ALL)
+                .addFilter(new AllureRestAssured())
+                .setContentType(ContentType.JSON).build();
+    }
+}
