b49f13d3dda7d177ba3315cb9484d0d059a0870b
Anastasiia Fokina <stasechka11@yandex.ru>
2022-06-09 11:27:07 +0300

    Get orders list test

---
+++ src/test/java/GetOrdersListTest.java
@@ -0,0 +1,39 @@
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.Response;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ru.yandex.practicum.scooter.api.OrdersClient;
+import ru.yandex.practicum.scooter.api.model.order.Order;
+import ru.yandex.practicum.scooter.api.model.order.OrdersList;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static ru.yandex.practicum.scooter.api.model.order.Order.getRandomOrder;
+
+public class GetOrdersListTest {
+    Order order;
+    OrdersClient ordersClient;
+    static int orderTrack;
+
+    @Before
+    public void init() {
+        ordersClient = new OrdersClient();
+        order = getRandomOrder();
+        Response createResponse = ordersClient.createOrder(order);
+        orderTrack = ordersClient.getOrderTrack(createResponse);
+    }
+
+    @After
+    public void clear() {
+        ordersClient.cancelOrder(orderTrack);
+    }
+
+    @Test
+    @DisplayName("Check orders list is displayed")
+    public void getOrderListTest() {
+        List<OrdersList> ordersList = ordersClient.getOrdersList();
+        assertFalse(ordersList.isEmpty());
+    }
+}
